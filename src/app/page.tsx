81ee9175eae39027771dc3da605608693ce6d514
Abie Koshy <Abie2023>
2025-06-29 21:14:58 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import UserCard from "./UserCard";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
+export const dynamic = "force-dynamic";
 export default async function Home() {
   const users: User[] = await getAllUsers();
   return (
